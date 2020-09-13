e44194315d3ac3ddb5bb8e6ab38dbb020745add1
Sreedhara GS <sreedhargs89@gmail.com>
2020-09-13 12:01:11 +0530

    Adding image and href to product

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 const app = Vue.createApp({
     data() {
         return {
-            product: 'Socks'
+            product: 'Socks',
+            image: './assets/images/socks_green.jpg',
+            url: 'https://cdn.shopify.com/s/files/1/0247/0107/5549/products/STS0012E_clipped_rev_1_540x.png?v=1588681687'
         }
     }
 })
