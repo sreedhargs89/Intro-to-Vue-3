9ea7be9f7e11ddd86f26f4ddb8ea4fc96457b2c7
Sreedhara GS <sreedhargs89@gmail.com>
2020-09-13 15:32:03 +0530

    Disable button for out of stock items

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
 const app = Vue.createApp({
     data() {
         return {
-            cart:0,
+            cart: 0,
             product: 'Socks',
             brand: 'Vue Mastery',
             image: './assets/images/socks_blue.jpg',
-            inStock: false,
+            inStock: true,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
-              { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
-              { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
+                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
+                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
             ]
         }
     },
